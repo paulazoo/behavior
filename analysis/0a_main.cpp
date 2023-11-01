be619cbb419be9651a3f4f0097ca06e1e0414ee7
paulazoo <paulazhu@college.harvard.edu>
2023-11-01 17:48:31 -0400

    binaries are so much more organized

--- analysis/0a_main.cpp
+++ analysis/0a_main.cpp
@@ -4,6 +4,11 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
 #include <matio.h> // for reading .mat files into C
 
 using namespace std;
@@ -34,15 +39,21 @@ vector<double> slice_double_vec(const vector<double> &data, int start_i, int end
     return vector<double>(data.begin() + start_i, data.begin() + end_i);
 }
 
-int main() {
-    int beginning_samples_to_skip = 15460;
+int main(int argc, char** argv) {
+    // Inputs; argv[0] is file name
+    char* output_folder = argv[1]; // the output folder where the binaries will be saved e.g. ./Data/AnB1/B1_20231030/
+    char* matlab_filename = argv[2]; // the lever data .mat filename e.g. ./Data/AnB1/B1_20231030.mat
+    int beginning_samples_to_skip = atoi(argv[3]); // number of beginning samples to skip
+
+    // Create output folder directory
+    mkdir(output_folder, 0777);
 
     // Initialize MATIO library
     mat_t *matfp;
     matvar_t *matvar;
 
     // Open .mat file
-    matfp = Mat_Open("./Data/AnB1/B1_20231030.mat", MAT_ACC_RDONLY);
+    matfp = Mat_Open(matlab_filename, MAT_ACC_RDONLY);
     if ( nullptr == matfp ) {
        cerr << "Error opening MAT file" << endl;
        return EXIT_FAILURE;
@@ -54,6 +65,7 @@ int main() {
        cerr << "Error reading variable 'lever_data' from MAT file" << endl;
        return EXIT_FAILURE;
     }
+    // Check lever_data taken from .mat file
     cout << "lever_data from .mat size: " << (matvar->dims[0]) << "," << (matvar->dims[0]) << endl;
     cout << "lever_data from .mat rank: " << (matvar->rank) << endl;
 
@@ -71,46 +83,59 @@ int main() {
         cout << "lever_data vector with unused rows taken out size: " << lever_data.size() << endl;
 
         // Save the entire lever_data vector to a binary file
-        save_double_vec_to_bin(lever_data, "./Data/AnB1/B1_20231030_lever_data.bin");
+        save_double_vec_to_bin(lever_data, string(output_folder)+"full.bin");
         
-        // break lever_data vector into trial and ITI chunks
+        // Break lever_data vector into trial+subsequent ITI chunks ==================================
+        // initialize some variables first
         double previous_lever_value = lever_data[beginning_samples_to_skip];
         double lever_value = lever_data[beginning_samples_to_skip];
         int previous_switch_i = beginning_samples_to_skip;
         int num_switches = 0;
-
+        // iterating through lever values...
         for (size_t i = beginning_samples_to_skip; i < lever_data.size(); ++i) {
-
             lever_value = lever_data[i];
             
+            // check if a jump from >2000 to <2000 happened between previous lever value and current lever value
             if (lever_value < 2000 && previous_lever_value > 2000) {
                 // switched from 2500 to 500
                 cout << "finished a trialITI: " << i << \
                 " trialITI length: " << i - previous_switch_i << " samples = ~" << (i - previous_switch_i)/5000 << "s" << endl;
                 
+                // Get this trial+subsequent ITI as a separate chunk from the full lever_data
                 vector<double> lever_data_chunk = slice_double_vec(lever_data, previous_switch_i, i);
+                // Rescale ITI lever values back down to 0-1023 from 2000-2023
                 for (size_t j = 0; j < lever_data_chunk.size(); ++j) {
                     if (lever_data_chunk[j] > 2000) {
                         lever_data_chunk[j] = lever_data_chunk[j] - 2000;
                     }
                 }
-                save_double_vec_to_bin(lever_data_chunk, \
-                "./Data/AnB1/B1_20231030_trial"+to_string(num_switches)+".bin");
 
+                // save it to a .bin file to be read in python later
+                save_double_vec_to_bin(lever_data_chunk, \
+                string(output_folder)+"trial"+to_string(num_switches)+".bin");
+                
+                // keep track of total num_switchess
+                // a switch is when lever values jump from >2000 to <2000 since this should happen every time tStart is turned to HIGH again
                 num_switches = num_switches + 1;
+
+                // note the lever value index which will become the starting index for the next chunk
                 previous_switch_i = i;
             }
 
+            // save current lever value to previous lever value so comparisons can be made
             previous_lever_value = lever_value;
         }
+        // ===========================================
 
+        // at the end, print out the total number of switches detected. This should align with the number of trials
         cout << "total switches: " << num_switches << endl;
 
     } else {
+        // uh.
         cerr << "The variable is not a 1D array of type double." << endl;
     }
     
-    // Free MATIO resources
+    // Free up MATIO resources
     Mat_VarFree(matvar);
     Mat_Close(matfp);
 
