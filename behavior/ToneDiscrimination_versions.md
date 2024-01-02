438900e77ffe65dd1bfe02982bb67e6b48b05b43
paulazoo <paulazhu@college.harvard.edu>
2024-01-02 17:02:02 -0500

    lever press and back

--- behavior/ToneDiscrimination_versions.md
+++ behavior/ToneDiscrimination_versions.md
@@ -26,8 +26,7 @@
 - no yellow light
 - left handside block against lever moving back
 - reward delivery click sound
-- __push down__
-- __white noise punishment sound at end of decision duration__
+- __push down and back__
 
 # version4:
 - no foreperiod
@@ -37,16 +36,5 @@
 - no yellow light
 - left handside block against lever moving back
 - reward delivery click sound
-- __push forward__
-- __white noise punishment sound at end of decision duration__
-
-# version_extra:
-- no foreperiod
-- ITI is sampled from a random uniform distribution set in durations.ITISettings
-- movement is defined as the passing of two absolute thresholds, noMvtThresh and mvtThresh (wrt MVT0 sampled at the start of the session), within a duration specified in durations.maxLeverPressDuration.
-- moving past noMvtThresh during the last second of ITI causes ITI to be extended until no more movement for a second
-- no yellow light
-- left handside block against lever moving back
-- __new lever (could be analog, new, or scavenged)__
-- reward delivery click sound
+- __push forward and back__
 
---
+++ behavior/helpers/leverMVT/detectLeverPressAndBack.m
@@ -0,0 +1,86 @@
+function [ARDUINO,leverPressAndBack,ESC] = detectLeverPress(ARDUINO,params,escapeKey)
+    % DETECT LEVER MOVEMENT ABOVE THRHESHOLD DEFINED IN PARAMS
+    % ARDUINO is a structure with fields
+    %   in = serial port for input arduino
+    %   out = serial port for output arduino
+    %   idx = current idx number. Increased everytime arduino in is sampled
+    %   t0 = reference start time to evaluate data from
+    %   data = data read from arduino:
+    %        d(1) = absolute time
+    %        d(2) = lever value
+    %        d(3) = lickspout value
+    % params = [detectionDuration MVT0 noMvtThresh mvtThresh maxLeverPressDuration];
+    
+    detectionDuration = params(1);
+    MVT0 = params(2);
+    noMvtThresh = params(3);
+    mvtThresh = params(4);
+    maxLeverPressDuration = params(5);
+
+    leverPress = false;
+    leverPressAndBack = false;
+
+    ESC = true;
+    
+    % Check if movement for a fixed duration
+    started_below_noMvtThresh = false;
+    time_since_noMvtThresh = 0;
+    detectionStart_time = tic;
+    current_time = toc(detectionStart_time);
+    while leverPress == false && ESC && current_time < detectionDuration
+        % update time
+        current_time = toc(detectionStart_time);
+
+        % update arduinoIN data
+        ARDUINO.data(ARDUINO.idx,:) = readArduino(ARDUINO.in, ARDUINO.t0);
+
+        % if we previously started below the noMvtThresh
+        if started_below_noMvtThresh == true
+            time_since_noMvtThresh = current_time - noMvtThresh_time;
+
+            % check if we made it past mvtThresh and did a lever press
+            if ARDUINO.data(ARDUINO.idx,2) > (MVT0+mvtThresh)
+                leverPress = true;
+            
+            % otherwise check if the time for completing a leverPress Mvt is up
+            elseif time_since_noMvtThresh > maxLeverPressDuration
+                started_below_noMvtThresh = false;
+                time_since_noMvtThresh = 0;
+                disp('ran out of time to pass both noMvtThresh and mvtThresh.')
+            end
+        end
+
+        % check if we have started below the noMvtThresh
+        if ARDUINO.data(ARDUINO.idx,2) < (MVT0+noMvtThresh)
+            started_below_noMvtThresh = true;
+            noMvtThresh_time = current_time;
+        end
+
+        % for escaping
+        [~,~,keyCode] = KbCheck;
+        ESC = keyCode(escapeKey) == 0;
+
+        % increment Arduino.idx
+        ARDUINO.idx = ARDUINO.idx+1;
+    end
+    
+    while leverPress == true && ESC && current_time < detectionDuration
+        % update time
+        current_time = toc(detectionStart_time);
+
+        % update arduinoIN data
+        ARDUINO.data(ARDUINO.idx,:) = readArduino(ARDUINO.in, ARDUINO.t0);
+
+        % check if we come back below noMvtThresh
+        if ARDUINO.data(ARDUINO.idx,2) < (MVT0+noMvtThresh)
+            leverPressAndBack = true;
+        end
+
+        % for escaping
+        [~,~,keyCode] = KbCheck;
+        ESC = keyCode(escapeKey) == 0;
+
+        % increment Arduino.idx
+        ARDUINO.idx = ARDUINO.idx+1;
+    end
+    
